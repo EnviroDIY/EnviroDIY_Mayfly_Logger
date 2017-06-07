bc7dbdb8941ab6098683de2877206bf3618f1bc7
Beth Fisher <beth.fisher001@gmail.com>
2017-06-07 11:26:21 -0500

    comments for demo understanding

--- examples/blink_demo_1/blink_demo_1.ino
+++ examples/blink_demo_1/blink_demo_1.ino
@@ -1,6 +1,12 @@
+//These are the outside libraries to include in the sketch. 
+//They must be in your Arduino/libraries directory for the sketch to run.
+//Strictly speaking neither of these are required for the blink demo.
 #include <Arduino.h>
 #include <Wire.h>
 
+//This list located before the "void" sets defines the variables for the sketch. 
+//This is useful for assigning pins or delay time. For example, Mayfly has LEDs at
+//pins 8 and 9.
 int delayTime = 500;   //milliseconds
 
 void setup ()
