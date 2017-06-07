79e8bd8afe586192e6bc7ffe08bc340db9ee1288
Beth Fisher <beth.fisher001@gmail.com>
2017-06-07 12:00:58 -0500

    included tutorial text for blink demo

--- examples/blink_demo_1/blink_demo_1.ino
+++ examples/blink_demo_1/blink_demo_1.ino
@@ -1,33 +1,53 @@
-//These are the outside libraries to include in the sketch. 
-//They must be in your Arduino/libraries directory for the sketch to run.
-//Strictly speaking neither of these are required for the blink demo.
+
+/*
+ * This is blink demo designed for the EnviroDIY Mayfly logger. The Mayfly has two LED lights 
+ * at pins 8 and 9. To run this demo you will need to first connect to the Mayfly by 
+ * specifying your board under Tools>Board (select appropriate Mayfly board) and 
+ * specifying which communication port to use under Tools>Port. This demo assumes you
+ * have installed the proper drivers to connect to the Mayfly.
+ * 
+ * Once connected to the mayfly, you need to Verify/Compile the sketch by clicking the
+ * checkmark at the top, left corner of the Arudino app window. If there is an error,
+ * it will appear (in orange) below. If there are no errors the window color will not change
+ * and it will read "Done compiling." Always compile before uploading a sketch to your Arduino.
+ * Once compiled, click on the right arrow button to upload the sketch to the Mayfly. 
+ * If it worked, one of the LED lights on the board will begin to blink.  
+ */
+
+//Sketches begin with a list of the outside libraries needed to run the sketch. 
+//They must be in your .../Arduino/libraries directory for the sketch to run.
+//Neither of these are required for the blink demo, but they are commonly required in sketches. 
+//After you run the sketch, try commenting them out and re-run the sketch.
 #include <Arduino.h>
 #include <Wire.h>
 
-//This list located before the "void" sets defines the variables for the sketch. 
-//This is useful for assigning pins or delay time. For example, Mayfly has LEDs at
-//pins 8 and 9.
-int delayTime = 500;   //milliseconds
 
+//This variables list is located before the "void" sets defines the variables for the sketch. 
+//Variables are useful for assigning information once at the beginning of a sketch, 
+//because most sketches will need to repeat information like pin locations. 
+//Notice that the sketch repeats the "delayTime" four times. 
+//"int" specifies interger data type.
+//Try modifying the delay time and LED pin and re-running the sketch.
+int delayTime = 500;   //milliseconds, so 1000 = one second
+int LED_BUILTIN = 9;   //Mayfly has LEDs at pins 8 (green) and 9 (red)
+
+
+// the "setup" function runs once when you press reset or power the board
 void setup ()
 {
-    pinMode(8, OUTPUT);
-    pinMode(9, OUTPUT);
-    Serial.begin(57600);
-    delay(1000);
-    Serial.println("Mayfly: Blink demo 1");
-    delay(1000);
+    pinMode(LED_BUILTIN, OUTPUT);
+    Serial.begin(57600);  //establishes data rate for serial transmission
+    delay(delayTime);
+    Serial.println("Mayfly: Blink demo 1"); //sends a message to the Serial Monitor
+    delay(delayTime);
 }
 
+
+// the "loop" function runs over and over again, forever
 void loop ()
 {
-    digitalWrite(8, HIGH);
-    digitalWrite(9, HIGH);
-
-    delay(delayTime);
-
-    digitalWrite(8, LOW);
-    digitalWrite(9, LOW);
-
-    delay(delayTime);
+  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  delay(delayTime);                       // wait for the time specified in delayTime variable
+  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  delay(delayTime);                       // wait for the time specified in delayTime variable
 }
