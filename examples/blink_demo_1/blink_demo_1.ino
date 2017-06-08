203d757f9118b591662b5b4cc44f8ea1f8a203e1
Beth Fisher <beth.fisher001@gmail.com>
2017-06-08 10:52:51 -0500

    improved readability of demo for first-time users

--- examples/blink_demo_1/blink_demo_1.ino
+++ examples/blink_demo_1/blink_demo_1.ino
@@ -4,14 +4,15 @@
  * Introduces use of variables. 
  * Includes Serial Monitor readout in setup function.
  * 
- * This is blink demo designed for the EnviroDIY Mayfly logger. The Mayfly has two LED lights 
- * at pins 8 and 9. To run this demo you will need to first connect to the Mayfly by 
- * specifying your board under Tools>Board (select appropriate Mayfly board) and 
- * specifying which communication port to use under Tools>Port. This demo assumes you
- * have installed the proper drivers to connect to the Mayfly. If you need a resource for 
- * getting started with Arduino, the tutorial by Adafruit at 
+ * This is blink demo is designed for first-time users of Arduino and the EnviroDIY Mayfly logger. 
+ * This demo assumes you have installed the proper drivers to connect to the Mayfly. If you need a 
+ * resource for getting started with Arduino, the tutorial by Adafruit at 
  * https://learn.adafruit.com/series/ladyadas-learn-arduino
  * will get you connected and running a blink demo in about an hour.
+ * 
+ * To run this demo you will need to first connect to the Mayfly by specifying your board under 
+ * Tools>Board (select appropriate Mayfly board) and specifying which communication port to use 
+ * under Tools>Port. 
  * 
  * Once connected to the mayfly, you need to Verify/Compile the sketch by clicking the
  * checkmark at the top, left corner of the Arudino app window. If there is an error,
@@ -19,6 +20,8 @@
  * and it will read "Done compiling." Always compile before uploading a sketch to your Arduino.
  * Once compiled, click on the right arrow button to upload the sketch to the Mayfly. 
  * If it worked, one of the LED lights on the board will begin to blink.  
+ * 
+ * The Mayfly has two LED lights located at pins 8 and 9. 
  * 
  original sketch:
  modified 8 May 2014 by Scott Fitzgerald
@@ -30,9 +33,10 @@
 
 //Sketches begin with a list of the outside libraries needed to run the sketch. 
 //They must be in your .../Arduino/libraries directory for the sketch to run.
-//These are not required for the blink demo, but they are commonly required in sketches. 
+//Arduino IDE automatically includes Arduino.h, but other IDEs require that you "include" it.
+//Wire.h is required for I2C (Inter-integrated Circuit) and TWI (Two-Wire Interface) connections.
 //After you run the sketch, try commenting them out and re-run the sketch.
-#include <Arduino.h>
+#include <Arduino.h>  
 #include <Wire.h>
 
 
@@ -43,7 +47,7 @@
 //"int" specifies interger data type.
 //Try modifying the delay time and LED pin and re-running the sketch.
 int delayTime = 1000;   //milliseconds, so 1000 = one second
-int LED_BUILTIN = 9;   //Mayfly has LEDs at pins 8 (green) and 9 (red)
+int LED_BUILTIN = 9;    //Mayfly has LEDs at pins 8 (green) and 9 (red)
 
 
 // The "setup" function runs once when you press reset or power the board. 
@@ -51,8 +55,8 @@ int LED_BUILTIN = 9;   //Mayfly has LEDs at pins 8 (green) and 9 (red)
 // the function simply performs an action. 
 void setup ()
 {
-    pinMode(LED_BUILTIN, OUTPUT);  // all you need to set up the LED blink
-    Serial.begin(57600);  //establishes data rate for serial transmission
+    pinMode(LED_BUILTIN, OUTPUT);           // all you need to set up the LED blink
+    Serial.begin(57600);                    //establishes data rate for serial transmission
     delay(delayTime);
     Serial.println("Mayfly: Blink demo 1"); //sends a message to the Serial Monitor (click magnifying glass)
     delay(delayTime);
@@ -63,8 +67,8 @@ void setup ()
 // so "void" is once again used in the function declaration. 
 void loop ()
 {
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
+  digitalWrite(LED_BUILTIN, HIGH);        // turn the LED on (HIGH is the voltage level)
   delay(delayTime);                       // wait for the time specified in delayTime variable
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
+  digitalWrite(LED_BUILTIN, LOW);         // turn the LED off by making the voltage LOW
   delay(delayTime);                       // wait for the time specified in delayTime variable
 }
