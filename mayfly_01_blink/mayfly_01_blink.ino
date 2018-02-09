b0f2cb6bbcbd1c530e4ecd9b969d559d7dc46e73
Beth Fisher <beth.fisher001@gmail.com>
2018-02-08 23:17:35 -0600

    Copying sketches

---
+++ mayfly_01_blink/mayfly_01_blink.ino
@@ -0,0 +1,30 @@
+//Mayfly LED Blink demo
+#include <Arduino.h>
+#include <Wire.h>
+
+// Declare variables with a type and initialize with an inital value assignment
+int i = 1;
+int delaytime = 200;  // time in milliseconds
+
+// The setup function runs once when you press reset or power the board
+void setup()           // the void keyword is used in function declarations to indicate that no information will be returned outside the function.
+{
+    Serial.begin(57600);
+    Serial.println("EnviroDIY Mayfly: Blink demo");
+    pinMode(8, OUTPUT);
+    pinMode(9, OUTPUT);
+}
+
+// The loop function runs over and over again forever
+void loop() {
+
+  digitalWrite(8, HIGH);
+  digitalWrite(9, LOW);
+  delay(delaytime);
+  digitalWrite(8, LOW);
+  digitalWrite(9, HIGH);
+  delay(delaytime);
+
+  Serial.println(i);
+  i = i+1;            // Increment the value of i by +1, or reassign a new value to i that is 1 greater than the previous value
+}
